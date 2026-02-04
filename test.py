a5ff67411e4edc416f5475407647f57060fc13b3
Vincent <vincenttueminh@gmail.com>
2026-02-03 19:52:03 -0500

    some stuff

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 from Vincent import *
 
+# AiModel.launch()
 print( AiModel.get_all_models() )
 
-
-# prompt = AiModel.Prompt( "Which ollama downloadable ollama model can use tools (like web search in this prompt) and process (\"see\") images like this one ?" )
-# prompt.loadImage( "C:/Users/Vinad/Pictures/Screenshots/Capture d’écran 2025-08-21 112258.png" )
-# model = AiModel.get_closest_model( "gpt" )
-# # for model in AiModel.get_all_models():
-# print( model )
-# response, t, w = AiModel.ask( prompt, model, True, True )
-# print( "Model :", model )
-# print( "Thinking :", t )
-# print( "Web search :", w )
-# print( response )
-# print( "------------------------------------------------------------------------------------------------------" )
-
-
-# System.clear_cache()
+prompt = AiModel.Prompt( "Dit moi dans quel bac ce déchet va, poubelle, composte ou recyclage" )
+prompt.loadImage( "C:/Users/Vinad/Downloads/326190.jpg" )
+response = AiModel.ask( prompt, AiModel.get_closest_model( "gemma" ), False, False )
+print( response[0] )
